2e252f9f1ddb510f2f9849949631c9cddd670c6b
Conor Walsh <conorwalsh0703@gmail.com>
2019-07-29 22:10:23 +0100

    created all templates

--- posts/views.py
+++ posts/views.py
@@ -39,4 +39,4 @@ def create_or_edit_a_post(request, pk= None):
     
     else:
         form = BlogPostForm(instance=post)
-    return render(request, 'blogpostform/html', {'form': form})
+    return render(request, 'blogpostform.html', {'form': form})
